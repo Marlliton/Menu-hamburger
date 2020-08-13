b9bcb2fbcbc45df47ba342e48bb3e13983ee64ca
Marlliton Souza <marlliton.souza1@gmail.com>
2020-08-13 04:06:37 -0300

    Primeira versao do menu

---
+++ index.js
@@ -0,0 +1,17 @@
+
+document.querySelector('.menu').addEventListener('click', botao)
+
+function botao() {
+    const span1 = document.querySelector('#span1').classList
+    const span2 = document.querySelector('#span2').classList
+    const span3 = document.querySelector('#span3').classList
+    span1.toggle('transform-before')
+    span2.toggle('transform-center')
+    span3.toggle('transform-after')
+    esconder()
+}
+
+function esconder() {
+    const nav = document.querySelector('#navegacao').classList
+    nav.toggle('esconder')
+}
